e67bc7d25ddced9ffcfe790c331ef2e12524e7dd
richielollis <richiedlollis@gmail.com>
2022-09-30 20:51:21 -0400

    get and post framework

--- server.js
+++ server.js
@@ -2,3 +2,18 @@ const express = require("express");
 const notes = require("./db/db.json");
 
 console.log(notes);
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.get("/notes", (req, res) => {});
+
+app.get("*", (req, res) => {});
+
+app.get("/api/notes", (req, res) => {});
+
+app.post("/api/notes", (req, res) => {});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
